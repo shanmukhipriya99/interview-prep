2a3687d5dbfba5ca9d85c9c062c6fe2a9598896f
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-26 21:51:38 -0400

    Update two-sum.js

--- 001. Two Sum/two-sum.js
+++ 001. Two Sum/two-sum.js
@@ -6,6 +6,7 @@
 
 // <========== Solution-1 ===============>
 var twoSum = function(nums, target) {
+    let result = [];
     for(let i=0; i<nums.length;i++) {
         for(let j=i+1;j<nums.length;j++) {
             if(nums[i] + nums[j] == target) {
@@ -27,4 +28,5 @@ var twoSum = function(nums, target) {
         }
         numMap.set(nums[i], i);
     }
+    return [];
 };
