d5f38eb13e0ad02a7508c37f6a568706f11641df
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-26 22:48:22 -0400

    Update index.js

--- 003. Roman To Integer/index.js
+++ 003. Roman To Integer/index.js
@@ -1,1 +1,25 @@
-
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const roman = {
+        'I': 1, 
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    let number = 0;
+    for(let i=0; i<s.length; i++) {
+        if(i+1 < s.length && roman[s[i]] < roman[s[i+1]]) {
+            number += (roman[s[i+1]] - roman[s[i]])
+            i++
+        } else {
+            number += roman[s[i]]
+        }
+    }
+    return number;
+};
