4e94e7409f5a00861d90ab9af4099115dfae29f2
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-08-28 00:05:17 -0400

    Update index.js

--- 010. Search Insert Position/index.js
+++ 010. Search Insert Position/index.js
@@ -3,9 +3,30 @@
  * @param {number} target
  * @return {number}
  */
+
+// <======== Solution - 1 =================>
+
 var searchInsert = function(nums, target) {
     if(nums.includes(target)) {
       return nums.indexOf(target);
     } 
-    return [...nums, target].sort((a, b) => a-b).indexOf(target);
+    return [...nums, target].sort((a, b) => a-b).indexOf(target);  // sort() => O(nlogn)
+};
+
+// <========= Solution - 2 ============>
+
+var searchInsert = function(nums, target) {
+    let low = 0, high = nums.length - 1;
+    let mid = 0;
+    while(low <= high) {
+      mid = Math.floor((low+high)/2);
+      if(nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return low;
 };
